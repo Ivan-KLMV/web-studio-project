09e2cb7a6f14d96b1d27e5fc0cd245af118d0af1
Ivan Klimov <klimov128@gmail.com>
2023-04-03 14:07:28 +0300

    Add filter animation

--- js/filter.js
+++ js/filter.js
@@ -1,5 +1,6 @@
 const filterBtnList = document.querySelector('.filter');
 const filterBtnArr = document.querySelectorAll('.filter__btn');
+const listOfCards = document.querySelector('.projects');
 const cards = document.querySelectorAll('.projects__item');
 
 filterBtnList.addEventListener('click', cardsFilter);
@@ -14,7 +15,8 @@ function cardsFilter(evt) {
   if (filterItem === 'all') {
     removeActivClass();
     addActiveClass(evt);
-    return makeClearFilter();
+    makeClearFilter();
+    return;
   }
   removeActivClass();
   addActiveClass(evt);
@@ -22,22 +24,48 @@ function cardsFilter(evt) {
   makeFilter(cards, filterItem);
 }
 
+// function showCards() {
+//   [...cards].map((card) => card.classList.add('show'));
+// }
+
 function makeClearFilter() {
-  [...cards].map(
-    (card) => {
-      card.style.display = 'block';
-    }
-    // card.removeAttribute('hidden')
-  );
+  listOfCards.style.columnGap = '30px';
+
+  [...cards].map((card) => {
+    card.classList.remove('hide');
+    card.style.margin = 0;
+  });
 }
 
 function makeFilter(cards, filter) {
+  const filteredElements = [...cards].filter(
+    (card) => card.dataset.item === filter
+  );
+  const lastIndex = filteredElements.length - 1;
+  listOfCards.style.columnGap = 0;
+
   [...cards]
     .filter((card) => card.dataset.item !== filter)
     .map((card) => {
-      card.style.display = 'none';
-      // card.setAttribute('hidden', true);
+      card.classList.add('hide');
     });
+
+  if (filteredElements.length === 2) {
+    filteredElements[0].style.marginRight = '15px';
+    filteredElements[lastIndex].style.marginLeft = '15px';
+    return;
+  }
+
+  if (filteredElements.length > 2) {
+    filteredElements[0].style.marginRight = '15px';
+    filteredElements[lastIndex].style.marginLeft = '15px';
+    const middleElements = filteredElements.splice(1, lastIndex - 1);
+    middleElements.forEach((el) => {
+      el.style.marginRight = '15px';
+      el.style.marginLeft = '15px';
+      return;
+    });
+  }
 }
 
 function removeActivClass() {
