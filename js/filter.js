09082e9557f85b9525139f5f79f60bca1cd156d6
Ivan Klimov <klimov128@gmail.com>
2023-04-03 15:43:08 +0300

    update filter.js and css

--- js/filter.js
+++ js/filter.js
@@ -50,21 +50,50 @@ function makeFilter(cards, filter) {
       card.classList.add('hide');
     });
 
+  if (listOfCards.getBoundingClientRect().width <= 480) {
+    listOfCards.style.gap = 0;
+    listOfCards.style.display = '-webkit-box';
+    filteredElements.forEach((el) => (el.style.marginBottom = '30px'));
+    return;
+  }
+
   if (filteredElements.length === 2) {
     filteredElements[0].style.marginRight = '15px';
     filteredElements[lastIndex].style.marginLeft = '15px';
     return;
   }
 
-  if (filteredElements.length > 2) {
+  if (
+    filteredElements.length > 2 &&
+    listOfCards.getBoundingClientRect().width >= 1100
+  ) {
     filteredElements[0].style.marginRight = '15px';
     filteredElements[lastIndex].style.marginLeft = '15px';
+
     const middleElements = filteredElements.splice(1, lastIndex - 1);
     middleElements.forEach((el) => {
       el.style.marginRight = '15px';
       el.style.marginLeft = '15px';
+      console.log(listOfCards.getBoundingClientRect().width);
       return;
     });
+  }
+
+  if (listOfCards.getBoundingClientRect().width > 480) {
+    filteredElements.forEach((el, i) => {
+      if (i % 2 === 0) {
+        console.log(el);
+        el.style.margin = 0;
+        el.style.marginRight = '15px';
+      }
+
+      if (i % 2 !== 0) {
+        console.log(el);
+        el.style.margin = 0;
+        el.style.marginLeft = '15px';
+      }
+    });
+    return;
   }
 }
 
