42c5e816102cd6415d96752545c7b10425b1489b
Ivan Klimov <klimov128@gmail.com>
2023-05-19 19:28:27 +0300

    fix filter on portfolio

---
+++ js/index.js
@@ -0,0 +1,34 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//   var filterContainer = document.querySelector('.filter-container');
+//   var filterizr = new Filterizr(filterContainer);
+// });
+const options = {
+  callbacks: {
+    onFilteringStart: function () {
+      const filterBtnList = document.querySelector('.filter');
+
+      filterBtnList.addEventListener('click', cardsFilter);
+      function cardsFilter(evt) {
+        if (evt.target.nodeName !== 'BUTTON') {
+          return;
+        }
+        removeActivClass();
+        addActiveClass(evt);
+      }
+    },
+  },
+  gutterPixels: 30,
+};
+const filterizr = new Filterizr('.filter-container', options);
+
+function addActiveClass(evt) {
+  evt.target.classList.add('filter__btn--is_active');
+}
+
+function removeActivClass() {
+  const filterBtnArr = document.querySelectorAll('.filter__btn');
+
+  [...filterBtnArr].map((btn) => {
+    btn.classList.remove('filter__btn--is_active');
+  });
+}
